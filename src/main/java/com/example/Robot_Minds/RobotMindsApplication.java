8ddd8d7b474a82f081ffeb307e0efa6990ae71d3
Khaled Ibrahim <khaled.abdo11@gmail.com>
2026-07-08 17:44:14 +0200

    Initial commit

---
+++ src/main/java/com/example/Robot_Minds/RobotMindsApplication.java
@@ -0,0 +1,13 @@
+package com.example.Robot_Minds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RobotMindsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RobotMindsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Robot_Minds/RobotMindsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Robot_Minds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RobotMindsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
